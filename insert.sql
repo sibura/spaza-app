56bac1d42da07610e5c7c421e8ae6633f02bb077
Nwabisa <linkie@projectCodex.co>
2015-06-26 11:01:24 +0200

    done

---
+++ insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO categories (category_name) VALUES ("Dairy Products");
+INSERT INTO categories (category_name) VALUES ("Bakery Products");
+INSERT INTO categories (category_name) VALUES ("Can Food");
+INSERT INTO categories (category_name) VALUES ("Cosmetics");
+INSERT INTO categories (category_name) VALUES ("Soup");
+INSERT INTO categories (category_name) VALUES ("Confectionaries");
+INSERT INTO categories (category_name) VALUES ("Valentine Goodies");
+INSERT INTO categories (category_name) VALUES ("Bulk");
+INSERT INTO categories (category_name) VALUES ("Fruits");
+INSERT INTO categories (category_name) VALUES ("Cold Beverages");
+
