793e72565271c3f0806a1855fbb6b770df2c4390
sibura <sibusiso@projectcodex.co>
2015-07-14 16:14:43 +0200

    SQL JS products

---
+++ routes/SqlFunctions.js
@@ -0,0 +1,16 @@
+  exports.showProducts = function(req, res, next){
+  		req.getConnection(function(error, connection){
+  			if(error){
+  				return next(error);
+  			}
+
+			connection.query('SELECT * FROM products', [], function(error, results) {
+			    if (error) return next(error);
+				console.log(results);
+			    res.render( 'productList', {
+				product : results
+			    });
+			});
+  		});
+  };
+
