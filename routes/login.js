a9e57e4e23406245cc0bd9ef33ce7466ef221e3e
sibura <sibusiso@projectcodex.co>
2015-08-11 15:51:36 +0200

    modifying login

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,7 @@
-var loggin = function(req, res, next){
+exports.checkUser = function(req, res, next){
   if (req.session.user){
     return next();
   }
   // the user is not logged in redirect him to the login page
-  res.redirect('login', {layout: false});
-};
+  res.redirect('login');
+};  
--- server.js
+++ server.js
@@ -4,6 +4,7 @@
  bodyParser = require('body-parser'),
  myConnection = require('express-myconnection');
  var session = require('express-session');
+ //var cookieSession =require('cookie-session');
 
  var sqlfunctions = require('./routes/SqlFunctions');
  var sqlcategory = require('./routes/sqlcategory');
@@ -19,26 +20,71 @@
  var catEarning = require('./routes/CategEarnings');
  var profitables = require('./routes/allProfitables');
  var loggin = require('./routes/login');
-
- var dbOptions = {
-   host: 'localhost',
-   user: 'root',
-   password: 'coder123',
-   port: 3306,
-   database: 'SpazaApp'
- };
+// var server = app.listen('localhost');
+//console.log(loggin);
+var dbOptions = {
+ host: 'localhost',
+ user: 'root',
+ password: 'coder123',
+ port: 3306,
+ database: 'SpazaApp'
+};
 
 
    // create a route
    var app = express();
+   app.use(express.static('public'));
    app.use(myConnection(mysql, dbOptions, 'single'));
    app.engine('handlebars', exphbs({defaultLayout: 'main'}));
    app.set('view engine', 'handlebars');
    app.use(bodyParser.urlencoded({ extended: false }));
    app.use(bodyParser.json());
+   app.use(session({ 
+    secret : 'coder123'
+  }));
    var fs = require('fs');
+   app.get('/login', function (req, res) {
+    res.render('log',{layout:false});
+  });
+   app.post('/login', function (req, res) {
+    var input = JSON.parse(JSON.stringify(req.body))
+    if(input.username === 'sbuda' && input.password === 'coder123'){
+      req.session.user = input.username;
+      return res.redirect('home')
+    }
+    return res.redirect('login')
+    alert ("Login was successful");
+  });
 
+   app.get('/', function (req, res) {
+    if(req.session.user){
+      res.render('home')
+    }
+    else {
+      res.render('log');
+    }
 
+  });
+   app.get('/home', function (req, res, next) {
+     // body...
+     if(req.session.user){
+      return next()
+    }
+    return res.redirect('/login')
+  }, function (req, res) {
+    res.render('home')
+  });
+//app.use('/log', loggin.loggin);
+app.use(function(req, res, next){
+  console.log('in my middleware!');
+  if(req.session.user){
+   return  next();
+
+ }
+ else{
+  res.redirect('/login');
+}
+});
   //products
   app.get('/products', sqlfunctions.showProducts);
 
@@ -85,7 +131,7 @@
  app.get('/sales', sqlsales.showSales);
 
  app.get('/sales/edit/:Id', sqlsales.get);
- app.post('/sales/edit/:Id', sqlsales.update)
+ app.post('/sales/edit/:Id', sqlsales.update);
 //app.post('/sales/update/:Id', sqlsales.update);
 app.post('/sales/add', sqlsales.add);
 //this should be a post but this is only an illustration of CRUD - not on good practices
@@ -104,38 +150,34 @@ app.get('/showProfitables', profitables.profitableProdz);
 app.get('/showLeastCat', LeastPopCat.LeastCat);
 
 
-//app.use('/log', loggin.loggin);
-app.use(function(req, res, next){
-  console.log('in my middleware!');
-  //proceed to the next middleware component
-  next();
-});
 
-app.get('/users', function(req, res){
-  var userData = userService.getUserData();
-  res.render('users', userData)
-});
 
-app.get('/login', function (req, res) {
-    res.render('log')
- // res.render('home',{cat:leastPopularCateg});
 
-});
+/*app.get('/login', function (req, res) {
+    res.render('');
+  });*/
 
-app.post('/add_product', function(req, res){
- var formData = req.body;
+
+
+ app.post('/add_product', function(req, res){
+   var formData = req.body;
  //console.log(formData.product_name);
  res.render('product', {product_name :  formData.product_name});
 });
   //app.use(express.static('public'));
+  app.get('/users', function(req, res){
+    var userData = userService.getUserData();
+    res.render('users', userData)
+  });
 
-  app.get('/', function (req, res) {
-    res.render('home')
- // res.render('home',{cat:leastPopularCateg});
 
-});
 
-  app.use(express.static('public'));
+  //these are the logout
+  app.get('/logout', function(req, res){
+    delete req.session.user;
+    res.redirect('login');
+  });
+
 
   app.get('/showProdlist', ListOfProdz.showProdsgroup);  
 
